fc08bdac2fd9cb79ae4b7650548a15f5e37bc544
angyy11 <angyy-wm19@student.tarc.edu.my>
2020-08-09 12:50:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a1111/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.a1111;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seond);
+        Intent intent = getIntent();
+    }
+}
